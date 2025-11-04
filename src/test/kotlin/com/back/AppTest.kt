5a947904bd35775efa3cdf61c40b7c4b4b88cac6
junseokPP <pjs981130@gmail.com>
2025-11-04 10:09:28 +0900

    17,18강 : AppTest작성시작

--- src/test/kotlin/com/back/AppTest.kt
+++ src/test/kotlin/com/back/AppTest.kt
@@ -1,15 +1,13 @@
 package com.back
 
-import org.junit.jupiter.api.DisplayName
-import kotlin.run
+import org.assertj.core.api.Assertions.assertThat
 import kotlin.test.Test
 import kotlin.test.assertContains
 
 class AppTest {
 
     @Test
-    @DisplayName("명언 작성")
-    fun t1() {
+    fun `명언 작성`() {
         val result = TestRunner.run(
             """
             등록
@@ -21,5 +19,27 @@ class AppTest {
         assertContains(result, "명언: ")
         assertContains(result, "작가: ")
         assertContains(result, "1번 명언이 등록되었습니다.")
+    }
+
+    @Test
+    fun `명언 목록`() {
+        val result = TestRunner.run(
+            """
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                과거에 집착하지 마라.
+                작자미상
+                목록
+                """
+        )
+        println("result: $result")
+
+        assertThat(result)
+            .contains("번호 / 작가 / 명언")
+            .contains("----------------------")
+            .contains("2 / 작자미상 / 과거에 집착하지 마라.")
+            .contains("1 / 작자미상 / 현재를 사랑하라.");
     }
 }
